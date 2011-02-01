e3d123f2653939ef40422a32a0cdf4120112b470
heman4t@gmail.com <heman4t@gmail.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-02-01 16:44:58 +0000

    Enhanced ACL with better modular understanding

--- trunk/library/trunk/application/controllers/ErrorController.php
+++ trunk/library/trunk/application/controllers/ErrorController.php
@@ -9,7 +9,7 @@ class ErrorController extends Zend_Controller_Action {
 			case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE :
 			case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER :
 			case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION :
-				
+			
 				// 404 error -- controller or action not found
 				$this->getResponse ()->setHttpResponseCode ( 404 );
 				$this->view->message = 'Page not found';
