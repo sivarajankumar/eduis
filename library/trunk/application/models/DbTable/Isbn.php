297969b09e88b42dbbddd4cabc369ee3830b47fc
heman4t <heman4t@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-03-04 05:59:51 +0000

    Search grid (Fixes issue 5). OPAC is introduced. OPAC has now issue/return status of book

--- library/trunk/application/models/DbTable/Isbn.php
+++ library/trunk/application/models/DbTable/Isbn.php
@@ -1,33 +1,36 @@
 <?php
 class Lib_Model_DbTable_Isbn extends Libz_Base_Model {
-	const TABLE_NAME = 'isbn';
-	protected $_name = 'isbn';
-	
-	public static function getIsbnDetails($isbn_id) {
-		$sql = self::getDefaultAdapter()->select ()->from ( self::TABLE_NAME )->where ( 'isbn_id = ?', $isbn_id );
-		return $sql->query ()->fetch ();
-	}
-	
-	
-	public function getBookGdata($isbn_id) {
-		$objGdata = new Zend_Gdata_Books ( );
-		$query = $objGdata->newVolumeQuery ();
-		$isbnno = 'isbn:' . $isbn_id;
-		$query->setQuery ( $isbnno );
-		$feed = $objGdata->getVolumeFeed ( $query );
-		$thumbnail_img = null;
-		foreach ( $feed as $entry ) {
-			$thumbnailLink = $entry->getThumbnailLink ();
-			if ($thumbnailLink) {
-				$thumbnail = $thumbnailLink->href;
-			} else {
-				$thumbnail = null;
-			}
-			$preview = $entry->getPreviewLink ()->href;
-			$thumbnail_img = (! $thumbnail) ? '' : '<a href="' . $preview . '"><img src="' . $thumbnail . '"/></a>';
-		}
-		return $thumbnail_img;
-	}
 
+    const TABLE_NAME = 'isbn';
+
+    protected $_name = 'isbn';
+
+    public static function getIsbnDetails ($isbn_id) {
+        $sql = self::getDefaultAdapter()->select()
+            ->from(self::TABLE_NAME)
+            ->where('isbn_id = ?', $isbn_id);
+        return $sql->query()->fetch();
+    }
+
+    public function getBookGdata ($isbn_id) {
+        $objGdata = new Zend_Gdata_Books();
+        $query = $objGdata->newVolumeQuery();
+        $isbnno = 'isbn:' . $isbn_id;
+        $query->setQuery($isbnno);
+        $feed = $objGdata->getVolumeFeed($query);
+        $thumbnail_img = null;
+        foreach ($feed as $entry) {
+            $thumbnailLink = $entry->getThumbnailLink();
+            if ($thumbnailLink) {
+                $thumbnail = $thumbnailLink->href;
+            } else {
+                $thumbnail = null;
+            }
+            $preview = $entry->getPreviewLink()->href;
+            $thumbnail_img = (! $thumbnail) ? '' : '<a href="' . $preview .
+             '"><img src="' . $thumbnail . '"/></a>';
+        }
+        return $thumbnail_img;
+    }
 }
 ?>
