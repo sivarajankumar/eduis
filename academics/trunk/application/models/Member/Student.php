ade88d998e696a0c2ff4948f6ad580dad4de1b9f
2308016 <2308016@aceambala.com>
2011-03-10 11:55:48 +0000

    student_model_attendence created

--- academics/trunk/application/models/Member/Student.php
+++ academics/trunk/application/models/Member/Student.php
@@ -1,6 +1,7 @@
 <?php
 class Acad_Model_Member_Student extends Acad_Model_Member_Generic {
-     
+  protected $_mapper;
+    
     /**
      * Set roll number of student
      * 
@@ -26,5 +27,69 @@ class Acad_Model_Member_Student extends Acad_Model_Member_Generic {
         return self::getMemberId();
     }
     
+    public function getAttendence($dateFrom=null,$dateUpto=null)
+    {
+        
+    }
 	
+/**
+     * Set data mapper
+     * 
+     * @param  mixed $mapper 
+     * @return Acad_Model_Student
+     */
+    public function setMapper($mapper)
+    {
+        $this->_mapper = $mapper;
+        return $this;
+    }
+
+    /**
+     * Get data mapper
+     *
+     * Lazy loads Acad_Model_StudentMapper instance if no mapper registered.
+     * 
+     * @return Acad_Model_StudentMapper
+     */
+    public function getMapper()
+    {
+        if (null === $this->_mapper) {
+            $this->setMapper(new Acad_Model_StudentMapper());
+        }
+        return $this->_mapper;
+    }
+	  /**
+     * Save the current entry
+     * 
+     * @return void
+     */
+    public function save()
+    {
+        $this->getMapper()->save($this);
+    }
+
+    /**
+     * Find an entry
+     *
+     * Resets entry state if matching id found.
+     * 
+     * @param  int $id 
+     * @return Acad_Model_Student
+     */
+    public function find($id)
+    {
+        $this->getMapper()->find($id, $this);
+        return $this;
+    }
+
+    /**
+     * Fetch all entries
+     * 
+     * @return array
+     */
+    public function fetchAll()
+    {
+        return $this->getMapper()->fetchAll();
+    }
 }
+    
