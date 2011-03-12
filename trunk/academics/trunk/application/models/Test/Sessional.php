dedf1c68a55c697af227c25fc3a269308cce56f8
2308016@aceambala.com <2308016@aceambala.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-03-12 07:32:24 +0000

    sessional class updated

--- trunk/academics/trunk/application/models/Test/Sessional.php
+++ trunk/academics/trunk/application/models/Test/Sessional.php
@@ -20,6 +20,7 @@ class Acad_Model_Test_Sessional extends Acad_Model_Test_Generic
      * @return Acad_Model_Test_Sessional
      */
     public function setMapper($mapper){
+        $this->_mapper=$mapper;
         
         return $this;
     }
@@ -29,7 +30,30 @@ class Acad_Model_Test_Sessional extends Acad_Model_Test_Generic
      * @return Acad_Model_Test_SessionalMapper $mapper - Sessional Mapper
      */
     public function getMapper(){
+        if (null === $this->_mapper) {
+            $this->setMapper(new Acad_Model_Test_SessionalMapper());
+        }
         return $this->_mapper;
     }
+
+    /**
+     * Save the current entry
+     * 
+     * @return void
+     */
+    public function save()
+    {
+        $this->getMapper()->save($this);
+    }
+ /**
+     * Fetch all entries
+     * 
+     * @return array
+     */
+    public function fetchAll()
+    {
+        return $this->getMapper()->fetchAll();
+    }   
+        
 }
 ?>
