a54a717e431fcc1e45036c63f546b119a4fc4eb3
2307002 <2307002@aceambala.com>
2011-03-19 06:20:35 +0000

    Acad_Model_Dbtabe_TestInfo Inroduced.

---
+++ academics/trunk/application/models/DbTable/TestInfo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 
+ * test_info dbtable file
+ * @author udit sharma
+ *
+ */
+class Acad_Model_DbTable_TestInfo extends Acadz_Base_Model
+{
+     protected $_name = 'test_info';
+}
+?>
