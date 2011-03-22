37fb9f0c809005c35ae8ec623af075e3e570c2d4
2307002 <2307002@aceambala.com>
2011-03-22 05:55:38 +0000

    Test models updated, Sessioanl Mapper partially done. Sessional Model and Test Model done.

--- academics/trunk/application/models/Test/Generic.php
+++ academics/trunk/application/models/Test/Generic.php
@@ -1,7 +1,7 @@
 <?php
 /** 
- * @author Administrator
- * 
+ * @author Administrator, udit sharma
+ * @version 3.0
  * 
  */
 abstract class Acad_Model_Test_Generic
@@ -11,305 +11,365 @@ abstract class Acad_Model_Test_Generic
      * Test info id
      * @var int
      */
-    protected $_testInfoId;
+    protected $_test_info_id;
     
     /**
-     * Department
+     * Department id
      * @var string
      */
-    protected $_department;
+    protected $_department_id;
      
     /**
-     * Degree
+     * Degree id
      * @var string
      */
-    protected $_degree;
+    protected $_degree_id;
      
      
     /**
-     * Semester
+     * Semester id
      * @var int
      */
-    protected $_semester;
+    protected $_semester_id;
     
     
     /**
-     * subject
+     * Subject code
      * @var string
      */
-    protected $_subject;
+    protected $_subject_code;
+    
+    /**
+     * Subject name
+     * @var string
+     */
+    protected $_subject_name;
     
     
     /**
-     * Test number
+     * Test type id
+     * @var string
+     * Can be overridden in derived classes.
+     */
+    protected $_test_type_id;
+    
+    
+    /**
+     * Test id
      * @var int
      */
-    protected $_testNumber;
-    
+    protected $_test_id;
     
     /**
-     * Test type
-     * @var string
+     * Time of conduct or submission(in case of assignments)
+     * @var str
      */
-    protected $_testType;
+    protected $_time;
      
+    /**
+     * Date of announcement
+     * @var date
+     */
+    protected $_date_of_announcement;
     
     /**
      * Date of conduct
      * @var date
      */
-    protected $_conductDate;
+    protected $_date_of_conduct;
     
     
     /**
-     * Max Marks
+     * Max marks
      * @var int
+     * Can be overridden by derived class
      */
-    protected $_maxMarks;
+    protected $_max_marks;
      
      
     /**
-     * Min marks
+     * Pass marks
      * @var int
+     * Can be overridden by derived class
      */
-    protected $_minMarks;
+    protected $_pass_marks;
     
     
     /**
      * Remarks
      * @var string
      */
-    protected $_remark;
-     
+    protected $_remarks;
+
+    /**
+     * Is optional
+     * @var small int
+     * Default value 
+     * 0 => compulsary
+     * 1 => optional 
+     */
+    protected $_is_optional = 0;
+    
+    /**
+     * Default pass marks
+     * @var int 
+     * Can be overridden in derived classes.
+     */
+    protected $_default_pass_marks;
+    
+    /**
+     * Default max marks
+     * @var int
+     * Can be overridden in derived classes.
+     */
+    protected $_default_max_marks;
     
     /**
      * Set test info id
      * @param int $testInfoId - test info id
      * @return Acad_Model_Test_Generic
      */
-    public function setTestInfoId($testInfoId){
-        $this->_testInfoId=$testInfoId;        
+    public function setTest_info_id($testInfoId){
+        $this->_test_info_id=$testInfoId;        
         return $this;
     }
     
     
     /**
      * Get test info id
-     * @return int $testInfoId - test info id
+     * @return int $_test_info_id - test info id
      */
-    public function getTestInfoId(){
-        return $this->_testInfoId;
+    public function getTest_info_id(){
+        return $this->_test_info_id;
     }
     
     
     /**
-     * Set department
-     * @param string $department - department
+     * Set department id
+     * @param string $departmentid - department id
      * @return Acad_Model_Test_Generic
      */
-    public function setDepartment($department){
-        $this->_department=$department;
+    public function setDepartment_id($departmentid){
+        $this->_department_id=$departmentid;
         return $this;
     }
     
     /**
-     * Get department
-     * @return string $department - department
+     * Get department id
+     * @return string $_department_id - department id
      */
-    public function getDepartment(){
-        return $this->_department;
+    public function getDepartment_id(){
+        return $this->_department_id;
     }
     
     
     /**
-     * Set degree
-     * @param string $degree - degree
+     * Set degree id
+     * @param string $degreeid - degree id
      * @return Acad_Model_Test_Generic
      */
-    public function setDegree($degree){
-        $this->_degree=$degree;
+    public function setDegree_id($degreeid){
+        $this->_degree_id=$degreeid;
         return $this;
     }
     
     /**
-     * Get degree
-     * @return string $degree - degree
+     * Get degree id
+     * @return string $_degree_id - degree id
      */
-    public function getDegree(){
-        return $this->_degree;
+    public function getDegree_id(){
+        return $this->_degree_id;
     }
-    
-     
      
     /**
-     * Set semester
-     * @param int $semester - semester
+     * Set semester id
+     * @param int $semesterid - semester id
      * @return Acad_Model_Test_Generic
      */
-    public function setSemester($semester){
-        $this->_semester=$semester;
+    public function setSemester_id($semesterid){
+        $this->_semester_id=$semesterid;
         return $this;
     }
     
     /**
-     * Get semester
-     * @return int $semester - semester
+     * Get semester id
+     * @return int $_semester_id - semester id
      */
-    public function getSemester(){
-        return $this->_semester;
+    public function getSemester_id(){
+        return $this->_semester_id;
     }
     
-     
-     
     /**
-     * Set subject
-     * @param string $subject - subject
+     * Set subject code
+     * @param array
      * @return Acad_Model_Test_Generic
      */
-    public function setSubject($subject){
-        $this->_subject=$subject;
+    public function setSubject_code($subjectcode){
+        $this->_subject_code=$subjectcode;
         return $this;
     }
     
     /**
-     * Get subject
-     * @return string $subject - subject
+     * Get subject code
+     * @return string $_subject_code - subject
      */
-    public function getSubject(){
-        return $this->_subject;
+    public function getSubject_code(){
+        return $this->_subject_code;
     }
     
-     
-     
     /**
-     * Set number
-     * @param int $testNumber - number
+     * Set subject name
+     * @param array
      * @return Acad_Model_Test_Generic
      */
-    public function setTestNumber($testNumber){
-        $this->_testNumber=$testNumber;
+    public function setSubject_name($subjectname){
+        $this->_subject_name=$subjectname;
         return $this;
     }
     
     /**
-     * Get number
-     * @return int $testNumber - number
+     * Get subject name
+     * @return string $_subject_name - subject
      */
-    public function getTestNumber(){
-        return $this->_testNumber;
+    public function getSubject_name(){
+        return $this->_subject_name;
     }
     
-     
+    
     /**
-     * Set test type
-     * @param string $testType - test type
+     * Set test type id
+     * @param string $testtypeid - test type id
      * @return Acad_Model_Test_Generic
      */
-    public function setTestType($testType){
-        $this->_testType=$testType;
+    public function setTest_type_id($testtypeid){
+        $this->_test_type_id=$testtypeid;
         return $this;
     }
     
     /**
-     * Get test type
-     * @return string $testType - test type
+     * Get test type id
+     * @return string $test_type_id - test_type_id
      */
-    public function getTestType(){
-        return $this->_testType;
+    public function getTest_type_id(){
+        return $this->_test_type_id;
     }
     
      
-     
     /**
-     * Set date of conduct
-     * @param date $conductDate - date of conduct
+     * Set test id
+     * @param int $testid - test id
      * @return Acad_Model_Test_Generic
      */
-    public function setConductDate($conductDate){
-        $this->_conductDate=$conductDate;
+    public function setTest_id($testid){
+        $this->_test_id=$testid;
         return $this;
     }
     
     /**
-     * Get date of conduct
-     * @return date $conductDate - date of conduct
+     * Get test id
+     * @return string $_test_id - test id
      */
-    public function getConductDate(){
-        return $this->_conductDate;
+    public function getTest_id(){
+        return $this->_test_id;
     }
     
-    
-/**
-     * Set Max Marks
-     * @param int $maxMarks - Max Marks
-     * @return Acad_Model_generic
-     */
-    public function setMaxMarks($maxMarks){
-        $this->_maxMarks = $maxMarks;
-        return $this;
-    }
-    
-    /**
-     * Get Max Marks
-     * @return int $maxMarks - Max Marks
-     */
-    public function getMaxMarks(){
-        return $this->_maxMarks;
-    }
-    
-     
-     
-    /**
-     * Set min marks
-     * @param int $minMarks - min marks
-     * @return Acad_Model_generic
-     */
-    public function setMinMarks($minMarks){
-        $this->_minMarks = $minMarks;
-        return $this;
-    }
-    
-    /**
-     * Get min marks
-     * @return int $minMarks - min marks
-     */
-    public function getMinMarks(){
-        return $this->_minMarks;
-    }
-    
-    /**
-     * Time of conduct
-     * @var str
-     */
-    protected $_time;
-     
-     
-    /**
-     * Set Time of conduct
-     * @param str $time - Time of conduct
+	/**
+     * Set Time of conduct/submission
+     * @param str $time - Time of conduct/submission
      * @return Acad_Model_Test_Generic
      */
     public function setTime($time){
-        
+        $this->_time = $time;
         return $this;
     }
     
     /**
-     * Get Time of conduct
+     * Get Time of conduct/assignments
      * @return str $time - Time of conduct
      */
     public function getTime(){
         return $this->_time;
     }
     
+    /**
+     * Set date of announcement
+     * @param date $dateofannouncement - date of announcement
+     * @return Acad_Model_Test_Generic
+     */
+    public function setDate_of_coduct($dateofannouncement){
+        $this->_date_of_announcement = $dateofannouncement;
+        return $this;
+    }
+    
+    /**
+     * Get date of announcement
+     * @return date $date_of_announcement - date of announcement
+     */
+    public function getDate_of_announcement(){
+        return $this->_date_of_announcement;
+    }
+    
+    /**
+     * Set date of conduct
+     * @param date $dateofconduct - date of conduct
+     * @return Acad_Model_Test_Generic
+     */
+    public function setDate_of_conduct($dateofconduct){
+        $this->_date_of_conduct = $dateofconduct;
+        return $this;
+    }
+    
+    /**
+     * Get date of conduct
+     * @return date $date_of_conduct - date of conduct
+     */
+    public function getDate_of_conduct(){
+        return $this->_date_of_conduct;
+    }
+    
+	/**
+     * Set max marks
+     * @param int $maxmarks - max marks
+     * @return Acad_Model_generic
+     */
+    public function setMax_marks($maxmarks){
+        $this->_max_marks = $maxmarks;
+        return $this;
+    }
+    
+    /**
+     * Get max marks
+     * @return int $_max_marks - max marks
+     */
+    public function getMax_marks(){
+        return $this->_max_marks;
+    }
+    
+    /**
+     * Set pass marks
+     * @param int $passmarks - pass marks
+     * @return Acad_Model_generic
+     */
+    public function setPass_marks($passmarks){
+        $this->_pass_marks = $passmarks;
+        return $this;
+    }
+    
+    /**
+     * Get pass marks
+     * @return int $_pass_marks - pass marks
+     */
+    public function getPass_marks(){
+        return $this->_pass_marks;
+    }    
      
     /**
      * Set remarks
      * @param string $remark - remarks
      * @return Acad_Model_Test_Generic
      */
-    public function setRemark($remark){
-        
+    public function setRemark($remarks){
+        $this->_remarks = $remarks;
         return $this;
     }
     
@@ -318,9 +378,59 @@ abstract class Acad_Model_Test_Generic
      * @return string $remark - remarks
      */
     public function getRemark(){
-        return $this->_remark;
+        return $this->_remarks;
     }
     
-
+	/**
+     * Set is optional
+     * @param string $isoptional - is optional
+     * @return Acad_Model_Test_Generic
+     */
+    public function setIs_optional($isoptional){
+        $this->_is_optional = $isoptional;
+        return $this;
+    }
+    
+    /**
+     * Get is optional
+     * @return string $_is_optional - is optional
+     */
+    public function getIs_optional(){
+        return $this->_is_optionals;
+    }
+    
+	/**
+     * Set default pass marks
+     * @param string $defaultpassmarks - default pass marks
+     */
+    public function setDefault_pass_marks($defaultpassmarks){
+        $this->_default_pass_marks = $defaultpassmarks;
+        return $this;
+    }
+    
+	/**
+     * Get default pass marks
+     * @return string $_default_pass_marks - default pass marks
+     */
+    public function getDefault_pass_marks(){
+        return $this->_default_pass_marks;
+    }
+    
+	/**
+     * Set default max marks
+     * @param string $defaultmaxmarks - default max marks
+     */
+    public function setDefault_max_marks($defaultmaxmarks){
+        $this->_default_max_marks = $defaultmaxmarks;
+        return $this;
+    }
+    
+	/**
+     * Get default max marks
+     * @return string $_default_max_marks - default max marks
+     */
+    public function getDefault_max_marks(){
+        return $this->_default_max_marks;
+    }
 }
 ?>
--- academics/trunk/application/models/Test/SessionalMapper.php
+++ academics/trunk/application/models/Test/SessionalMapper.php
@@ -1,29 +1,37 @@
 <?php
+/**
+ * 
+ * Sessional Mapper
+ * @author udit sharma
+ * @version 3.0
+ *
+ */
 class Acad_Model_Test_SessionalMapper
 {
-/**
+    /**
      * @var Zend_Db_Table_Abstract
      */
+    
     protected $_dbTable;
-
+    
     /**
      * Specify Zend_Db_Table instance to use for data operations
      * 
      * @param  Zend_Db_Table_Abstract $dbTable 
      * @return Acad_Model_Test_SessionalMapper
      */
-    public function setDbTable($dbTable)
+    public function setDbTable ($dbTable)
     {
         if (is_string($dbTable)) {
             $dbTable = new $dbTable();
         }
-        if (!$dbTable instanceof Zend_Db_Table_Abstract) {
+        if (! $dbTable instanceof Zend_Db_Table_Abstract) {
             throw new Exception('Invalid table data gateway provided');
         }
         $this->_dbTable = $dbTable;
         return $this;
     }
-
+    
     /**
      * Get registered Zend_Db_Table instance
      *
@@ -31,10 +39,10 @@ class Acad_Model_Test_SessionalMapper
      * 
      * @return Zend_Db_Table_Abstract
      */
-    public function getDbTable()
+    public function getDbTable ()
     {
         if (null === $this->_dbTable) {
-            $this->setDbTable('Acad_Model_DbTable_Sessional');
+            $this->setDbTable('Acad_Model_DbTable_TestInfo');
         }
         return $this->_dbTable;
     }
@@ -42,7 +50,7 @@ class Acad_Model_Test_SessionalMapper
     /**
      * Save a sessional datesheet
      * 
-     * @param  Acad_Model_Test_SessionalMapper
+     * @param  array|Acad_Model_Test_Sessional
      * @return void
      */
     public function save ($sessional)
@@ -56,78 +64,145 @@ class Acad_Model_Test_SessionalMapper
             'testInfoId' => $sessional->getTestInfoId());
             if (null === ($testInfoId = $sessional->getTestInfoId())) {
                 unset($data['testInfoId']);
-  
-                 $sql='INSERT INTO '
-             . $this->quoteIdentifier($sessional, true)
-             . ' (' . implode(', ', $data) . ')';
-             return $this->getDbTable()->getAdapter()->query($sql);
+                $sql = 'INSERT INTO ' . $this->quoteIdentifier($sessional, true) .
+                 ' (' . implode(', ', $data) . ')';
+                return $this->getDbTable()
+                    ->getAdapter()
+                    ->query($sql);
             } else {
                 $set[] = $this->quoteIdentifier($data, true) . ' = ';
                 $where = $this->_whereExpr($where);
                 /**
-         * Build the UPDATE statement
-         */
-        $sql = "UPDATE "
-             . $this->quoteIdentifier($sessional, true)
-             . ' SET ' . implode(', ', $set)
-             . (($where) ? " WHERE $where" : '');
-
-             return $this->getDbTable()->getAdapter()->query($sql);
-                
+                 * Build the UPDATE statement
+                 */
+                $sql = "UPDATE " . $this->quoteIdentifier($sessional, true) .
+                 ' SET ' . implode(', ', $set) .
+                 (($where) ? " WHERE $where" : '');
+                return $this->getDbTable()
+                    ->getAdapter()
+                    ->query($sql);
             }
-        } elseif (is_array($sessional)){
-            
-        if (null === ($testInfoId = $sessional->getTestInfoId())) {
+        } elseif (is_array($sessional)) {
+            if (null === ($testInfoId = $sessional->getTestInfoId())) {
                 unset($data['testInfoId']);
-  
-                 $sql='INSERT INTO '
-             . $this->quoteIdentifier($sessional, true)
-             . ' (' . implode(', ', $data) . ')';
-             return $this->getDbTable()->getAdapter()->query($sql);
+                $sql = 'INSERT INTO ' . $this->quoteIdentifier($sessional, true) .
+                 ' (' . implode(', ', $data) . ')';
+                return $this->getDbTable()
+                    ->getAdapter()
+                    ->query($sql);
             } else {
                 $set[] = $this->quoteIdentifier($data, true) . ' = ';
                 $where = $this->_whereExpr($where);
                 /**
-         * Build the UPDATE statement
-         */
-        $sql = "UPDATE "
-             . $this->quoteIdentifier($sessional, true)
-             . ' SET ' . implode(', ', $set)
-             . (($where) ? " WHERE $where" : '');
-
-             return $this->getDbTable()->getAdapter()->query($sql);
-                
-            }    
+                 * Build the UPDATE statement
+                 */
+                $sql = "UPDATE " . $this->quoteIdentifier($sessional, true) .
+                 ' SET ' . implode(', ', $set) .
+                 (($where) ? " WHERE $where" : '');
+                return $this->getDbTable()
+                    ->getAdapter()
+                    ->query($sql);
+            }
         } else {
             throw new Zend_Exception('oye, ye kya bheja hai??', Zend_Log::ERR);
         }
     }
+    /**
+     * Fecthes schedule of particular sessional if exists
+     * 
+     * Otherwise, it will create partial schedule for further completion
+     * @param Acad_Model_Test_Sessional
+     * @return array Acad_Model_Test_Sessional
+     *  
+     */
+    public function fetchSchedule (Acad_Model_Test_Sessional $sessional)
+    {
+        //$logger = Zend_Registry::get('logger');
+        $check = $this->fetchAll($sessional);
+        //$logger->debug($check);
+        if (0 != count($check)) {
+            return $check;
+        } else {
+            
+            
+            
+//            $sql = 'SELECT `subject_department`.`department_id`
+//    						, `subject_department`.`degree_id`
+//    						, `subject_department`.`semester_id`
+//  							, `subject_department`.`subject_code`
+//    						, `subject`.`subject_name`
+//    						, `test`.`test_type_id`
+//    						, `test`.`test_id`
+//    						, `test`.`is_optional`
+//    						, `test_type`.`default_pass_marks`
+//    						, `test_type`.`default_max_marks`
+//				   FROM `academics`.`subject_department`
+//    					    , `academics`.`subject`
+//    					    , `academics`.`test`
+//                   INNER JOIN `academics`.`test_type` 
+//                         ON (`test`.`test_type_id` = `test_type`.`test_type_id`)
+//                   WHERE (`subject_department`.`department_id` = "CSE"
+//                          AND `subject_department`.`degree_id` ="BTECH"
+//                          AND `subject_department`.`semester_id` =8
+//                          AND `test`.`test_type_id` ="SESS"
+//                          AND `test`.`test_id` =2
+//                          AND `subject_department`.`subject_code` =`subject`.`subject_code`);';
+            
+            $result = $this->getDbTable()->getDefaultAdapter()
+                                         ->select()
+                                         ->from
+                           ->query($sql)
+                           ->fetchAll();
+                           
+            if ($result != null) {
+                $entries = array();
+                foreach ($result as $row) {
+                    $entry = new Acad_Model_Test_Sessional();
+                    $entry->setOptions($row)
+                          ->setMapper($this);
+                    $entries[] = $entry;
+                }
+                return $entries;
+            } else {
+                return "Didn't worked out.";
+            }
+        }
+    }
     
     /**
+     * Fetches all the entries for perticular sessional
      * 
-     * Enter description here ...
-     * return array Acad_Model_Test_Sessional
+     * @param Acad_Model_Test_Sessional
+     * @return array Acad_Model_Test_Sessional
      */
-    public function fetchAll ()
+    public function fetchAll (Acad_Model_Test_Sessional $sessional)
     {
-        $resultSet = $this->getDbTable()->fetchAll();
-        $entries = array();
-        foreach ($resultSet as $row) {
-            $entry = new Acad_Model_Test_Sessional();
-            $entry->setTestInfoId($row->test_info_id)
-                ->setDepartment($row->department_id)
-                ->setDegree($row->degree_id)
-                ->setSemester($row->semester_id)
-                ->setSubject($row->subject_code)
-                ->setTestType($row->test_type_id)
-                ->setTestNumber($row->test_id)
-                ->setTime($row->time)
-                ->setConductDate($row->date_of_conduct)
-                ->setMaxMarks($row->max_marks)
-                ->setMinMarks($row->pass_marks)
-                ->setMapper($this);
-            $entries[] = $entry;
+        //$logger = Zend_Registry::get('logger');
+        $resultSet = $this->getDbTable()->getDefaultAdapter()
+            ->select()
+            ->from($this->getDbTable()->info('name'))
+            ->joinInner('subject', 
+        			    '`test_info`.`subject_code` = `subject`.`subject_code`', 
+        			    'subject_name')
+            ->where('department_id = ?', $sessional->getDepartment_id())
+            ->where('degree_id = ?', $sessional->getDegree_id())
+            ->where('semester_id = ?', $sessional->getSemester_id())
+            ->where('test_type_id = ?', $sessional->getTest_type_id())
+            ->where('test_id =?', $sessional->getTest_id())
+            ->query()
+            ->fetchAll();
+        //$logger->debug($resultSet);
+        if ($resultSet != NULL) {
+            $entries = array();
+            foreach ($resultSet as $row) {
+                $entry = new Acad_Model_Test_Sessional();
+                $entry->setOptions($row)
+                      ->setMapper($this);
+                $entries[] = $entry;
+            }
+            return $entries;
+        } else {
+            return null;
         }
-        return $entries;
     }
 }
