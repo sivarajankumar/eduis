1cfbcbd9864784984e7ecde56335099063fe8f39
2308016@aceambala.com <2308016@aceambala.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-03-26 06:09:13 +0000

    member - Faculty & test- testmarks is created.

--- trunk/academics/trunk/application/models/Member/Faculty.php
+++ trunk/academics/trunk/application/models/Member/Faculty.php
@@ -1,5 +1,37 @@
 <?php
-class Acad_Model_Member_Faculty extends Acad_Model_Member_Generic {
-    
+/** 
+ * @author Babita
+ * @version 3.0
+ * 
+ */
+class Acad_Model_Member_Faculty extends Acad_Model_Member_Generic
+{
+    /**
+     * Faculty Subjects
+     * @var string|int
+     */
+    protected $_subject_faculty;
+    /**
+     * Set Faculty Subjects
+     * @param string $_subject_faculty
+     * @return Acad_Model_Member_Faculty()
+     */
+    public function getFacultySubjects ($_subject_faculty)
+    {
+        
+        $sql = 'SELECT
+		subject_faculty.subject_code
+            , subject.subject_name
+        FROM
+            subject_faculty
+            INNER JOIN subject 
+                ON (subject_faculty.subject_code = subject.subject_code)
+            INNER JOIN subject_department 
+                ON (subject_department.subject_code = subject.subject_code)
+        WHERE (subject_faculty.staff_id = ?)';
+        /*$this->logger->log ( 'getFacultySubject($_subject_faculty)', Zend_Log::INFO );
+		$this->logger->log ( $result, Zend_Log::DEBUG );*/
+        return $this->getAdapter()->fetchAll($sql, $_subject_faculty);
+    }
 }
 ?>
