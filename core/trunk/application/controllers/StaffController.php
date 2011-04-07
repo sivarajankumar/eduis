1c516e95e8d8b5c23b4fa430683d24aa67463ade
heman4t <heman4t@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-04-07 18:51:47 +0000

    Get Staff basic information

--- core/trunk/application/controllers/StaffController.php
+++ core/trunk/application/controllers/StaffController.php
@@ -36,4 +36,39 @@ class StaffController extends Corez_Base_BaseController
                         ->setHttpResponseCode(400);
         }
     }
+    
+public function getinfoAction ()
+    {
+        $request = $this->getRequest();
+        $format = $request->getParam('format', 'json');
+        $staff_id = $this->getRequest()->getParam('staff_id');
+        if (isset($staff_id)) {
+            $result = Core_Model_DbTable_StaffPersonal::staffInfo($staff_id);
+            switch (strtolower($format)) {
+                case 'json':
+                    $this->_helper->json($result);
+                    return;
+                case 'jsonp':
+                    $callback = $request->getParam('callback');
+                    echo $callback . '(' . $this->_helper->json($result, false) .')';
+                    return;
+                case 'select' :/*
+					echo '<select>';
+					echo '<option>Select one</option>';
+					foreach ( $result as $key => $row ) {
+						echo '<option value="' . $row ['batch_start'] . '">' . $row ['batch_start'] . '</option>';
+					}
+					echo '</select>';*/
+					return;
+                default:
+                    $this->getResponse()
+                        ->setException('Unsupported format request')
+                        ->setHttpResponseCode(400);
+            }
+        } else {
+                    $this->getResponse()
+                        ->setException('Valid parameter(s) required')
+                        ->setHttpResponseCode(400);
+        }
+    }
 }
