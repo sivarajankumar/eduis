0745bc9e35b9910e5c5c85c925d28bdfee1a53a6
heman4t <heman4t@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-04-07 18:51:47 +0000

    Get Staff basic information

--- trunk/core/trunk/application/models/DbTable/StaffPersonal.php
+++ trunk/core/trunk/application/models/DbTable/StaffPersonal.php
@@ -14,4 +14,11 @@ class Core_Model_DbTable_StaffPersonal extends Corez_Base_Model
         $sql = 'SELECT initial,staff_id,first_name,last_name,department_id from staff_personal where staff_id = ?';
         return self::getDefaultAdapter()->fetchRow($sql, array($staff_id));
     }
+    
+
+    public static function staffInfo ($staff_id)
+    {
+        $sql = 'SELECT initial,first_name,middle_name,last_name,department_id from staff_personal where staff_id = ?';
+        return self::getDefaultAdapter()->fetchRow($sql, array($staff_id));
+    }
 }
