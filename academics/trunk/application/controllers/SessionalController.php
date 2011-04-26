d5724482a6546ccec239dd295d4ffcc1d730531f
heman4t <heman4t@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-04-26 09:09:03 +0000

    Manage Sessional

--- academics/trunk/application/controllers/SessionalController.php
+++ academics/trunk/application/controllers/SessionalController.php
@@ -16,13 +16,13 @@
 class SessionalController extends Acadz_Base_BaseController
 {
     protected $department_id;
-    
     public function init ()
     {
         $authInfo = Zend_Auth::getInstance()->getStorage()->read();
-        $this->department_id = 'APSC';
+        $this->department_id = $authInfo['department_id'];
         $this->view->assign('masterDepartment', $this->department_id);
-        $slaves = Acad_Model_DbTable_SemesterDegree::slaveDepartment($this->department_id);
+        $slaves = Acad_Model_DbTable_SemesterDegree::slaveDepartment(
+        $this->department_id);
         $this->view->assign('slaveDepartment', $slaves);
         parent::init();
     }
@@ -35,8 +35,7 @@ class SessionalController extends Acadz_Base_BaseController
         $this->_helper->layout()->enableLayout();
          // action body
     }
-    
-	/**
+    /**
      * @about Interface.
      */
     public function getsessionalAction ()
@@ -45,25 +44,21 @@ class SessionalController extends Acadz_Base_BaseController
         $test_id = $request->getParam('test_id');
         $format = $request->getParam('format', 'json');
         if (1) {
-            $values = array('department_id' => 'CSE', 
-            				'test_type_id' => 'SESS', 
-            				'test_id' => 1);
+            $values = array('department_id' => $this->department_id, 
+            'test_type_id' => 'SESS', 'test_id' => 1);
             $model = new Acad_Model_Test_Sessional($values);
             $sessionals = $model->fetchAll();
             $result = array();
             foreach ($sessionals as $key => $sessional) {
-                $result[$sessional->getSemester_id()][$sessional->getDate_of_conduct()][] = array($sessional->getTest_info_id(), 
-                $sessional->getDepartment_id(), 
-                $sessional->getDegree_id(), 
-                $sessional->getSubject_code(), 
-                $sessional->getSubject_name(), 
-                $sessional->getTime());
+                $result[$sessional->getSemester_id()][$sessional->getDate_of_conduct()][] = array(
+                $sessional->getTest_info_id(), $sessional->getDepartment_id(), 
+                $sessional->getDegree_id(), $sessional->getSubject_code(), 
+                $sessional->getSubject_name(), $sessional->getTime());
             }
             switch (strtolower($format)) {
                 case 'json':
                     $this->_helper->logger($result);
-                    echo $this->_helper->json($result,false);
-                    
+                    echo $this->_helper->json($result, false);
                     return;
                 default:
                     $this->getResponse()
@@ -92,73 +87,50 @@ class SessionalController extends Acadz_Base_BaseController
         $degree_id = $request->getParam('degree_id');
         $semester_id = $request->getParam('semester_id');
         $test_id = $request->getParam('test_id');
-        
-        
-        if ($request->isXmlHttpRequest() and $valid) 
-        {
+        if ($request->isXmlHttpRequest() and $valid) {
             $this->gridparam['page'] = $request->getParam('page', 1); // get the requested page
-            $this->gridparam['limit'] = $request->getParam('rows', 20); // rows limit in Grid
+            $this->gridparam['limit'] = $request->getParam('rows', 
+            20); // rows limit in Grid
             $this->gridparam['sidx'] = $request->getParam('sidx', 1); // get index column - i.e. user click to sort
-            $this->gridparam['sord'] = $request->getParam('sord','asc'); // sort direction
-            
-            $params = array('department_id'=>$department_id,
-                           'degree_id'=>$degree_id,
-                           'semester_id'=>$semester_id,
-                           'test_id'=>$test_id,
-                           'test_type_id'=>'SESS'
-                           );
+            $this->gridparam['sord'] = $request->getParam('sord', 
+            'asc'); // sort direction
+            $params = array('department_id' => $department_id, 
+            'degree_id' => $degree_id, 'semester_id' => $semester_id, 
+            'test_id' => $test_id, 'test_type_id' => 'SESS');
             $model = new Acad_Model_Test_Sessional($params);
             $result = $model->fetchSchedule();
+            $response = new stdClass();
+            $response->page = $this->gridparam['page'];
+            $response->total = 1;
+            
 
-                $this->_count = count($result);
-                $this->total_pages = 1;
-                $this->offset = 0;
-                $response = new stdClass();
-                $response->page = $this->gridparam['page'];
-                $response->total = $this->total_pages;
-                $response->records = $this->_count;
-
-                if(true == $result['exists']){
+            if ($result instanceof Zend_Exception) {
+                $this->getResponse()
+                    ->setException(new Exception('Invalid sessional parameter OR No entry exists', Zend_Log::ERR))
+                    ->setHttpResponseCode(400);
+            } elseif (true == $result['exists']) {
+                $response->records = count($result['data']);
+                foreach ($result['data'] as $key => $row) {
+                    $response->rows[$key]['id'] = $row->getTest_info_id();
+                    $response->rows[$key]['cell'] = array(
+                    $row->getSubject_code(), $row->getSubject_name(), 
+                    $row->getDate_of_conduct(), $row->getTime(), 
+                    $row->getMax_marks(), $row->getPass_Marks());
+                }
                 
-                    foreach ($result['data'] as $key => $row) 
-                    {
-                        $response->rows[$key]['id'] = $row->getTest_info_id();
-                        $response->rows[$key]['cell'] = array($row->getSubject_code(),
-                                                              $row->getSubject_name(),  
-                                                              $row->getDate_of_conduct(), 
-                                                              $row->getTime(), 
-                                                              $row->getMax_marks(), 
-                                                              $row->getPass_Marks()); 
-                                                              
-                    }
-            
-                    
-                    $this->_helper->json($response);
+                $this->_helper->json($response);
+            } else {
+                foreach ($result['data'] as $key => $row) {
+                    $response->rows[$key]['id'] = $row->getTest_info_id();
+                    $response->rows[$key]['cell'] = array(
+                    $row->getSubject_code(), $row->getSubject_name(), 
+                    $row->getDate_of_conduct(), $row->getTime(), 
+                    $row->getDefault_max_marks(), $row->getDefault_pass_Marks());
                 }
-                elseif(false == $result['exists']){
-                    foreach ($result['data'] as $key => $row) 
-                    {
-                        $response->rows[$key]['id'] = $row->getTest_info_id();
-                        $response->rows[$key]['cell'] = array($row->getSubject_code(),
-                                                              $row->getSubject_name(),  
-                                                              $row->getDate_of_conduct(), 
-                                                              $row->getTime(), 
-                                                              $row->getDefault_max_marks(), 
-                                                              $row->getDefault_pass_Marks());
-                    }
-            
-                    
-                    $this->_helper->json($response);
-                } 
-                else {
-                    $this->getResponse()
-                         ->setException('Non ajax request')
-                         ->setHttpResponseCode(400);
-                }
+                $this->_helper->json($response);
             }
-            
+        }
     }
-    
     /**
      * 
      * 
@@ -170,23 +142,20 @@ class SessionalController extends Acadz_Base_BaseController
         $this->_helper->layout()->enableLayout();
         $this->view->assign('masterDepartment', $this->department_id);
     }
-    
     /**
      * Get upcoming sessional information.
      * 
      * @about JSON data provider.
      */
-    public function getupcomingsessional(){
-        
+    public function getupcomingsessional ()
+    {
         $request = $this->getRequest();
-        
-        if($request->isXmlHttpRequest()){
+        if ($request->isXmlHttpRequest()) {
             $model = new Acad_Model_Test_Sessional();
-        }
-        else{
+        } else {
             $this->getResponse()
-                 ->setException('Non ajax request')
-                 ->setHttpResponseCode(400);
+                ->setException('Non ajax request')
+                ->setHttpResponseCode(400);
         }
     }
     /**
