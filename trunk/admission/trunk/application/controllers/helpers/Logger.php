ed83259a66accc659f8c53fbf815c53799884981
heman4t <heman4t@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-05-10 11:14:56 +0000

    Project prepared

---
+++ trunk/admission/trunk/application/controllers/helpers/Logger.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Logger helper class
+ * 
+ * @author 
+ * @version
+ *
+ */
+class Admsn_Controller_Helper_Logger extends Zend_Controller_Action_Helper_Abstract
+{
+    /**
+     * Logger instance
+     *
+     * @var Zend_Log
+     */
+    private $logger;
+    /**
+     * Constructor: initialize plugin loader with logger instance 
+     * depending on if configuration allows it
+     * @return void
+     */
+    public function __construct ()
+    {
+        if (Zend_Registry::isRegistered('logger')) {
+            $this->logger = Zend_Registry::get('logger');
+        } else {
+            throw new Exception("Logger is not in the Registry.");
+        }
+    }
+    function direct ($message, $msgType = Zend_Log::DEBUG)
+    {
+        if ($this->logger)
+            $this->logger->log($message, $msgType);
+    }
+    function __call ($name, $arguments)
+    {
+        if ($this->logger) {
+            try {
+                $this->logger->$name($arguments[0]);
+            } catch (Zend_Exception $e) {
+                self::direct($e->getMessage());
+            }
+        }
+    }
+}
