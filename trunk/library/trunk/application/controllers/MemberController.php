ef3c742947cc10bf4aca9e519e1593b917c35e97
2308009@aceambala.com <2308009@aceambala.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-07-31 05:18:35 +0000

    view of member account enabled

--- trunk/library/trunk/application/controllers/MemberController.php
+++ trunk/library/trunk/application/controllers/MemberController.php
@@ -1,29 +1,28 @@
 <?php
-
 /**
  * MemberController
  * 
  * @author 
  * @version 
  */
-
 class MemberController extends Libz_Base_BaseController {
-	/**
-	 * The default action - show the home page
-	 */
-	
-	public function getmemberinfoAction() {
-		self::getModel ();
-		$request = $this->getRequest ();
-		$member_id = $request->getParam ( 'member_id' );
-		$formatted = $request->getParam ( 'formatted' );
-		if ($member_id) {
-			$memberInfo = $this->model->getMemberInfo ( $member_id );
-			$issuedBook = Lib_Model_DbTable_IssueReturn::getIssuedDocumentCount ( $member_id );
-			$memberInfo ['issued'] = $issuedBook;
-			//$this->_helper->logger($memberInfo);
-			//$this->getResponse()->setRedirect($infoURL, 303);
-			/*
+
+    /**
+     * The default action - show the home page
+     */
+    public function getmemberinfoAction () {
+        self::getModel();
+        $request = $this->getRequest();
+        $member_id = $request->getParam('member_id');
+        $formatted = $request->getParam('formatted');
+        if ($member_id) {
+            $memberInfo = $this->model->getMemberInfo($member_id);
+            $issuedBook = Lib_Model_DbTable_IssueReturn::getIssuedDocumentCount(
+            $member_id);
+            $memberInfo['issued'] = $issuedBook;
+            //$this->_helper->logger($memberInfo);
+            //$this->getResponse()->setRedirect($infoURL, 303);
+            /*
 			if ($formatted and isset ( $memberDetail ['info'] )) {
 				$formattedDetail = array ();
 				foreach ( $memberDetail ['info'] as $key => $value ) {
@@ -36,44 +35,59 @@ class MemberController extends Libz_Base_BaseController {
 				}
 				$memberDetail ['info'] = $formattedDetail;
 			}*/
-			if ($memberInfo) {
-				$this->_helper->json ( $memberInfo );
-			} else {
-				$this->getResponse ()->setHttpResponseCode ( 400 );
-				echo 'Member "' . $member_id . '" not found.';
-			}
-		}
-	
-	}
-	// NOTE: THE ABOVE AND BELOW FUNCTIONS ARE ALMOST IDENTICAL!!
-	/**
-	 * Account Information of a member along with detailed list of books issued.
-	 */
-	public function getaccountdetailAction() {
-		self::getModel ();
-		$memberId = $this->getRequest ()->getParam ( 'member_id' );
-		if ($memberId) {
-			$memberDetail = $this->model->getMemberInfo ( $memberId );
-			if ($memberDetail) {
-				$issuedBook = Lib_Model_DbTable_IssueReturn::getIssuedDocumentList ( $memberId );
-				$memberDetail ['issued'] = $issuedBook;
-				//$this->_helper->logger ( $memberDetail );
-				$this->_helper->json ( $memberDetail );
-			} else {
-				$this->getResponse ()->setHttpResponseCode ( 400 );
-				echo ' ' . $memberId . ' is not a member';
-			}
-		
-		} else {
-			$this->getResponse ()->setHttpResponseCode ( 400 );
-			echo 'Insufficient parameters';
-		}
-	}
-	
-	public function accountinfoAction() {
-		$this->_helper->viewRenderer->setNoRender ( false );
-		$this->_helper->layout ()->enableLayout ();
-	}
+            if ($memberInfo) {
+                $this->_helper->json($memberInfo);
+            } else {
+                $this->getResponse()->setHttpResponseCode(400);
+                echo 'Member "' . $member_id . '" not found.';
+            }
+        }
+    }
+
+    // NOTE: THE ABOVE AND BELOW FUNCTIONS ARE ALMOST IDENTICAL!!
+    /**
+     * Account Information of a member along with detailed list of books issued.
+     */
+    public function getaccountdetailAction () {
+        self::getModel();
+        $memberId = $this->getRequest()->getParam('member_id');
+        if ($memberId) {
+            $memberDetail = $this->model->getMemberInfo($memberId);
+            if ($memberDetail) {
+                $issuedBook = Lib_Model_DbTable_IssueReturn::getIssuedDocumentList(
+                $memberId);
+                $memberDetail['issued'] = $issuedBook;
+                //$this->_helper->logger ( $memberDetail );
+                $this->_helper->json($memberDetail);
+            } else {
+                $this->getResponse()->setHttpResponseCode(400);
+                echo ' ' . $memberId . ' is not a member';
+            }
+        } else {
+            $this->getResponse()->setHttpResponseCode(400);
+            echo 'Insufficient parameters';
+        }
+    }
+
+    public function accountinfoAction () {
+        $this->_helper->viewRenderer->setNoRender(false);
+        $this->_helper->layout()->enableLayout();
+    }
+
+    public function indexAction () 
+    {
+        $this->_helper->viewRenderer->setNoRender(false);
+        $this->_helper->layout()->enableLayout();  
+/**
+ * 
+ * @todo dynamic id from auth
+ */
+        $id = '2308009';
+        $this->view->assign('id',$id);
+        
+    }
+
+ 
 }
 ?>
 
