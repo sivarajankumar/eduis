a9043ad9eab301b7dbf07c4ee76ab4605d88b522
2308010@aceambala.com <2308010@aceambala.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-08-02 10:38:05 +0000

    minor changes to parameters of fetchschedule function: 1)changed name of $numids to $numId 2)added a condition to check if $numIds has been set

--- trunk/academics/trunk/application/models/Assessment/Abstract.php
+++ trunk/academics/trunk/application/models/Assessment/Abstract.php
@@ -539,6 +539,10 @@ WHERE (`test_info_id` = ?)';
     public function fetchSchedule ($deg, $dep, $sem, $numIds = null)
     {
         $type = $this->test_type_id;
+        if(isset($numIds))
+        {
+            $numId=$numIds;
+        }
         $select = new Zend_Db_Table();
         $cols = array('test_info_id', 'is_optional', 'date_of_announcement', 
         'remarks', 'pass_marks', 'max_marks', 'date_of_conduct', 'time', 
@@ -554,22 +558,22 @@ WHERE (`test_info_id` = ?)';
             ->where('department_id =?', $dep)
             ->where('semester_id =?', $sem)
             ->where('test_type_id =?', $type);
-        $testIds = $this->getHighestTestIds($deg, $dep, $sem,$type, $numIds);
+        $testIds = $this->getHighestTestIds($deg, $dep, $sem, $type, $numId);
         foreach ($testIds as $value) {
-            $test[]="test_id='$value'";
+            $test[] = "test_id='$value'";
         }
-        $stringForWhereClause = implode(' or ',$test);
+        $stringForWhereClause = implode(' or ', $test);
         $sql->where($stringForWhereClause);
         $fetchAll = $adapterClass->getDefaultAdapter()
             ->query($sql)
             ->fetchAll();
-            return $fetchAll;
+        return $fetchAll;
     }
     /**
      *This function gets the test_ids corresponding to latest locked
      * or !locked Assessment as a/q to params
      */
-    public function getHighestTestIds ($deg, $dep, $sem, $type, $numIds = null,$lock = 1)
+    public function getHighestTestIds ($deg, $dep, $sem, $type, $numIds= null,$lock = 1)
     {
         $adapterClass = new Zend_Db_Table();
         $sql = $adapterClass->getDefaultAdapter()
@@ -580,7 +584,7 @@ WHERE (`test_info_id` = ?)';
             ->where('semester_id = ?', $sem)
             ->where('test_type_id = ?', $type)
             ->order('test_id DESC');
-            if ($lock === 0) {
+        if ($lock === 0) {
             $sql->where('is_locked = 0');
         }
         if ($lock === 1) {
@@ -592,7 +596,6 @@ WHERE (`test_info_id` = ?)';
         $fetchAll = $adapterClass->getDefaultAdapter()
             ->query($sql)
             ->fetchAll(Zend_Db::FETCH_COLUMN);
-            
         return array_values($fetchAll);
     }
 }
