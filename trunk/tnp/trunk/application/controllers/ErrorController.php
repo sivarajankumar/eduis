faf4da0482253bab15c7e0fca302ec64605faf57
heman4t@gmail.com <heman4t@gmail.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-09-06 09:49:54 +0000

    T&P module created

---
+++ trunk/tnp/trunk/application/controllers/ErrorController.php
@@ -0,0 +1,64 @@
+<?php
+class ErrorController extends Zend_Controller_Action
+{
+    public function errorAction ()
+    {
+        $this->_helper->layout()->disableLayout();
+        $this->getResponse()->clearBody(); 
+        $errors = $this->_getParam('error_handler');
+        $message = null;
+        if (! $errors) {
+            $this->view->message = 'You have reached the error page';
+            return;
+        }
+        switch ($errors->type) {
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
+                // 404 error -- controller or action not found
+                $this->getResponse()->setHttpResponseCode(404);
+                $message = 'Page not found';
+                break;
+            default:
+                // application error
+                $this->getResponse()->setHttpResponseCode(500);
+                $message = 'Application error';
+                break;
+        }
+        // Log exception, if logger available
+        $log = $this->getLog();
+        $code = $errors->exception->getCode();
+        if ($log) {
+            if (0 <= $code and $code <= 8) {
+                $log->log($errors->exception->getMessage(),$code);
+            } else {
+                $log->crit($errors->exception->getMessage());
+            }
+        }
+        if ($this->getRequest()->isXmlHttpRequest()) {
+            $this->_helper->viewRenderer->setNoRender();
+            $this->getResponse()->setHttpResponseCode(403);
+            if ('development' == strtolower(APPLICATION_ENV)) {
+                $this->getResponse()->setBody($errors->exception->getLine().$errors->exception->getFile().$errors->exception->getMessage().$errors->exception->getTraceAsString());
+            } else {
+                $this->getResponse()->setBody($errors->exception->getMessage());
+                
+            }
+        } else {
+            // conditionally display exceptions
+            if ($this->getInvokeArg('displayExceptions') == true) {
+                $this->view->exception = $errors->exception;
+            }
+            $this->view->message = $errors->exception->getMessage();
+            $this->view->request = $errors->request;
+        }
+        
+    }
+
+	public function getLog() {
+		if (Zend_Registry::isRegistered ( 'logger' )) {
+			$log = Zend_Registry::get ( 'logger' );
+			return $log;
+		}
+	}
+}
