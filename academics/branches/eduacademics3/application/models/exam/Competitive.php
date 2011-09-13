128a6fc12cbcef574bfcdcd7a615506bdfefc1ee
2308010 <2308010@aceambala.com>
2011-09-13 18:35:14 +0000

    changes in __get() functions code

--- academics/branches/eduacademics3/application/models/exam/Competitive.php
+++ academics/branches/eduacademics3/application/models/exam/Competitive.php
@@ -114,8 +114,17 @@ class Acad_Model_Exam_Competitive
         $method = 'get' . $name;
         if ('mapper' == $name || ! method_exists($this, $method)) {
             throw new Zend_Exception('Invalid property specified');
+        } else {
+            if (isset($this->$name)) {
+                return $this->$method();
+            } else {
+                $fetchMethodName = 'fetch' . $name;
+                if (method_exists($this->getMapper(), $fetchMethodName)) {
+                    $this->getMapper()->$fetchMethodName;
+                    return $this->$method();
+                }
+            }
         }
-        return $this->$method();
     }
     /**
      * used to init an object
