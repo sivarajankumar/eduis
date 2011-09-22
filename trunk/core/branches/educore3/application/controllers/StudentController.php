f5f0279fa900e13ac82b9effbae1029221414771
2308009@aceambala.com <2308009@aceambala.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-09-22 04:45:39 +0000

    getprofile action

--- trunk/core/branches/educore3/application/controllers/StudentController.php
+++ trunk/core/branches/educore3/application/controllers/StudentController.php
@@ -42,4 +42,32 @@ class StudentController extends Corez_Base_BaseController
             header("HTTP/1.1 400 Bad Request");
         }
     }
+    public function getprofileAction() 
+    {
+        $request = $this->getRequest();
+        //$rollno = $request->getparam('rollno');
+        $rollno = '2308009';
+        
+        $model = new Core_Model_Member_Student();       
+        $model->setStudent_roll_no($rollno);
+        $model->getMember_id();
+        $model->getStudentInfo();
+        
+       $info = array(
+       'roll_no'=>$model->getStudent_roll_no(),
+       'regn_no'=>$model->getReg_no(),
+       'first_name'=>$model->getFirst_name(),
+       'middle_name'=>$model->getMiddle_name(),
+       'last_name'=>$model->getLast_name(),
+       'gender'=>$model->getGender(),
+       'phone_no'=>$model->getContact_no(),
+       'dob'=>$model->getDob(),
+       'email'=>$model->getE_mail());
+       $this->view->assign('$personal_info',$info);
+       echo $info;
+        
+        
+     
+    }
+    
 }
