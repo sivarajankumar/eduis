f9db26900e98ba11bb85427e9cac237c3421dbf0
2308009@aceambala.com <2308009@aceambala.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-09-24 08:14:33 +0000

    getprofileAction() removed...and added to StudentPersonal controller

--- trunk/core/branches/educore3/application/controllers/StudentPersonalController.php
+++ trunk/core/branches/educore3/application/controllers/StudentPersonalController.php
@@ -11,4 +11,50 @@ class StudentPersonalController extends Corez_Base_BaseController
         $this->view->assign('controller', $this->_request->getControllerName());
         $this->view->assign('module', $this->_request->getModuleName());
     }
+    
+    /*
+     * RETURNS THE PROFILE OF GIVEN ROLL NUMBER
+     */
+     public function getprofileAction() 
+    {
+    
+        $model = new Core_Model_Member_Student();  
+        
+       /* if (Zend_Auth::getInstance()->hasIdentity()) 
+        {
+            $authInfo = Zend_Auth::getInstance()->getStorage()->read();
+            $this->_roll_no = $authInfo['roll_no'];
+            $this->_member_id = $authInfo['member_id'];
+            $model->setMember_id($this->_member_id);
+            $model->getStudentInfo();
+            
+        }
+        elseif ($this->getRequest()->getParam('roll_no'))
+        {
+            $model->setStudent_roll_no($this->_roll_no);
+            $model->getMember_id();
+            $model->getStudentInfo();
+        }
+        else 
+        {
+           throw new Exception('..................ABE ROLL NUMBER DAAL...........');
+        }
+        */
+       $this->_roll_no = '2308009'; 
+       $model->setStudent_roll_no($this->_roll_no);
+       $model->getMember_id();
+       $model->getStudentInfo();
+       $info = array(
+       'roll_no'=>$model->getStudent_roll_no(),
+       'regn_no'=>$model->getReg_no(),
+       'first_name'=>$model->getFirst_name(),
+       'middle_name'=>$model->getMiddle_name(),
+       'last_name'=>$model->getLast_name(),
+       'gender'=>$model->getGender(),
+       'phone_no'=>$model->getContact_no(),
+       'dob'=>$model->getDob(),
+       'email'=>$model->getE_mail());
+       print_r($info);
+       //$this->_helper->json($info);
+    }
 }
