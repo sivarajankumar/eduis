76394cdabe341b32e3c56e92ae83320ab1724790
2308009 <2308009@aceambala.com>
2011-09-26 08:39:49 +0000

    getprofile

--- core/branches/educore3/application/controllers/StudentController.php
+++ core/branches/educore3/application/controllers/StudentController.php
@@ -40,13 +40,62 @@ class StudentController extends Corez_Base_BaseController
                         ->setException('Unsupported format request')
                         ->setHttpResponseCode(400);
             }
-        } else {
+        } 
+        
+        else 
+        {
             header("HTTP/1.1 400 Bad Request");
         }
+        
+    }    
+    /*
+     * RETURNS THE PROFILE OF GIVEN ROLL NUMBER
+     */
+public function getprofileAction() 
+    {
+    
+        $model = new Core_Model_Member_Student(); 
+        $authInfo = Zend_Auth::getInstance()->getStorage()->read();
+        $member_id = $authInfo['member_id']; 
+       /* 
+       if (isset($member_id) 
+        {
+           
+            $model->setMember_id($member_id);
+            $model->getStudentInfo();
+            
+        }
+        elseif ($this->getRequest()->getParam('roll_no'))
+        {
+            $model->setStudent_roll_no($this->_roll_no);
+            $model->getMember_id();
+            $model->getStudentInfo();
+        }
+        else 
+        {
+           throw new Exception('..................ABE ROLL NUMBER DAAL...........',Zend_Log::ERR);
+        }
+        */
+       $this->_roll_no = '2308009'; 
+       $model->setStudent_roll_no($this->_roll_no);
+       $model->getMember_id();
+       $model->getStudentInfo();
+       $info = array(
+       'roll_no'=>$model->getStudent_roll_no(),
+       'regn_no'=>$model->getReg_no(),
+       'first_name'=>$model->getFirst_name(),
+       'middle_name'=>$model->getMiddle_name(),
+       'last_name'=>$model->getLast_name(),
+       'gender'=>$model->getGender(),
+       'phone_no'=>$model->getContact_no(),
+       'dob'=>$model->getDob(),
+       'email'=>$model->getE_mail());
+       print_r($info);
+       $callback = $this->getRequest()->getParam('callback');
+       echo $callback.'('.$this->_helper->json($info,false).')';
+       //$this->_helper->json($info);
+    }  
+        
     }
    
-        
-     
-    
-    
-}
+
