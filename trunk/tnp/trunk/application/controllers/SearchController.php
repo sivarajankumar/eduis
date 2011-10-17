826593347dc166a9f273089735a50855134f134d
2308009@aceambala.com <2308009@aceambala.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-10-17 09:47:45 +0000

    search frame added

---
+++ trunk/tnp/trunk/application/controllers/SearchController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * SearchController
+ * 
+ * @author
+ * @version 
+ */
+
+class SearchController extends Zend_Controller_Action
+{
+    /**
+     * The default action - show the home page
+     */
+    public function indexAction ()
+    {
+        // TODO Auto-generated SearchController::indexAction() default action
+    }
+    
+    public function fetchlist ()
+    {
+    	
+    }
+}
