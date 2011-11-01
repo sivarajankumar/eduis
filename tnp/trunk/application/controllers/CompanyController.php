f8f53f9eaf74624b1bc386c2e38bb6ed435991b1
2308015 <2308015@aceambala.com>
2011-11-01 14:40:36 +0000

    Company register controller created

---
+++ tnp/trunk/application/controllers/CompanyController.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * ComapnyController
+ * 
+ * @author
+ * @version 
+ */
+require_once 'Zend/Controller/Action.php';
+class CompanyController extends Zend_Controller_Action
+{
+    /**
+     * The default action - show the home page
+     */
+    protected $_company;
+    protected $_company_register;
+    protected $_company_contact;
+    
+    public function init ()
+    {
+        
+    }
+    
+    public function indexAction ()
+    {
+        $this->_helper->viewRenderer->setNoRender(true);
+        $this->_helper->layout()->enableLayout();
+    }
+    
+    public function registerAction ()
+    {
+       
+    }
+   
+    
+   /* public function setcontactAction ()
+    {
+        $request = $this->getRequest();
+        $params = array_diff($request->getParams(), $request->getUserParams());
+        $this->_helper->viewRenderer->setNoRender(TRUE);
+        $this->_helper->layout()->disableLayout();
+        foreach ($params as $colName => $value) {
+            $value = is_array($value) ? $value : htmlentities(trim($value));
+            $this->company_contact->$colName = $value;
+        }
+        echo 'Following information recieved:<br/>';
+        foreach ($params as $colName => $value) {
+            $value = is_array($value) ? var_export($value, true) : htmlentities(
+            trim($value));
+            echo '<b>' . ucwords(str_ireplace('_', ' ', $colName)) . '</b> : ' .
+             $value . '<br/>';
+        }
+    }*/
+    
+    public function saveAction ()
+    {
+    	$request = $this->getRequest();
+        $params = array_diff($request->getParams(), $request->getUserParams());
+        $this->_helper->viewRenderer->setNoRender(TRUE);
+        $this->_helper->layout()->disableLayout();
+    	
+        
+        
+        
+    }
+    
+    
+    
+    
+}
