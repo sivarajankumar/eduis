1308340acd40d70c3fc211ea27eb1f5170f6d82c
2308015 <2308015@aceambala.com>
2011-11-01 14:45:30 +0000

    job controller created

---
+++ tnp/trunk/application/controllers/JobController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * JobController
+ * 
+ * @author
+ * @version 
+ */
+require_once 'Zend/Controller/Action.php';
+class JobController extends Zend_Controller_Action
+{
+    /**
+     * The default action - show the home page
+     */
+	public function init(){
+		
+	}
+    public function indexAction ()
+    {}
+    public function registerAction ()
+    {
+        
+    }
+    public function saveAction ()
+    {
+        $request = $this->getRequest();
+        $params = array_diff($request->getParams(), $request->getUserParams());
+        $this->_helper->viewRenderer->setNoRender(TRUE);
+        $this->_helper->layout()->disableLayout();
+    	
+        
+        
+        
+    }
+    
+}
