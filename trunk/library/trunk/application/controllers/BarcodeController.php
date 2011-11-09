74ac5ca38b4736aa268f70780a7b30e903d9d98b
heman4t <heman4t@576e4884-5f5a-85fa-688c-129c97d2be85>
2011-11-09 07:36:39 +0000

    Enhanced Barcode Feature. And includes Students Barcode.

--- trunk/library/trunk/application/controllers/BarcodeController.php
+++ trunk/library/trunk/application/controllers/BarcodeController.php
@@ -7,7 +7,7 @@
  * @version 0.1
  */
 
-class BarcodeBookController extends Libz_Base_BaseController {
+class BarcodeController extends Libz_Base_BaseController {
 	/**
 	 * The default action - show the home page
 	 */
@@ -44,7 +44,7 @@ class BarcodeBookController extends Libz_Base_BaseController {
 			$this->_helper->logger('unacceptable accFrom');
 		}*/
 	}
-	
+
 
 	/**
 	 * The default action - show the home page
@@ -59,7 +59,34 @@ class BarcodeBookController extends Libz_Base_BaseController {
 			$this->view->assign ( 'isbn', $newISBN );
 		}
 	}
-	
+
+	/**
+	 * The default action - show the home page
+	 */
+	public function studentAction() {
+		$this->_helper->viewRenderer->setNoRender ( false );
+		$request = $this->getRequest ();
+		$department = $request->getParam ( 'department_id' );
+		$degree =  $request->getParam ( 'degree_id' );
+		$batch = $request->getParam ( 'batch' );
+		if (isset ( $degree ) and isset ( $department ) and isset ( $batch )) {
+			$client = new Zend_Http_Client('http://'.CORE_SERVER.'/batch/getbatchstudent'."?department_id=$department"."&degree_id=$degree"."&batch_id=$batch");
+			$client->setCookie ( 'PHPSESSID', $_COOKIE ['PHPSESSID'] );
+			$response = $client->request ();
+			if ($response->isError ()) {
+				$remoteErr = 'REMOTE ERROR: ('.$response->getStatus () . ') ' . $response->getHeader('Message');
+				throw new Zend_Exception ( $remoteErr, Zend_Log::ERR );
+			} else {
+				$jsonContent = $response->getBody ( $response );
+				$students = Zend_Json::decode($jsonContent);
+				$this->_helper->logger($jsonContent);
+	            $this->view->assign ( 'students', $students );
+	            $this->view->assign ( 'department', $department );
+	            $this->view->assign ( 'degree', $degree );
+	            $this->view->assign ( 'batch', $batch );
+			}
+		}
+	}
 	/**
 	 * Generate barcode for books.
 	 * 
