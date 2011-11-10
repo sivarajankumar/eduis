5d0c4179fd03bf7e58e13210b52536db0a27f57a
2308009 <2308009@aceambala.com>
2011-11-10 06:48:35 +0000

    search functional

--- core/branches/educore3/application/controllers/SearchController.php
+++ core/branches/educore3/application/controllers/SearchController.php
@@ -17,26 +17,28 @@ class SearchController extends Zend_Controller_Action
     {
         $request = $this->getRequest()->getParams();
         $model = new Core_Model_Member_Student();
-        $this->_helper->logger($request);
-        $dob_range = array('from' => $request['dob_from'],'to' =>$request['dob_to']);
-        $with_range = array('dob'=>$dob_range);
-        $without_range = array('gender'=>$request['gender'],
-                               'nationality_id'=>$request['nationality_id'],
-                               'cast_id'=>$request['cast']);
+        $request = $this->getRequest();
+        $params = array_diff($request->getParams(), $request->getUserParams());
+        $without_rangeKeys = array('gender'=>'',
+                               'nationality_id'=>'',
+                               'cast'=>'');
+        $with_rangeKeys = array('dob'=>'');
+        $without_range = array_intersect_key($params, $without_rangeKeys);
+        $with_range = array_intersect_key($params, $with_rangeKeys);
+        //$with_range = array_diff_key($params, $without_range);
         $this->_helper->logger($with_range);
+        $this->_helper->logger($without_range);
         $search_result = $model->search($without_range,$with_range);
         
+        $incomeKey = array('annual_income'=>''); 
+        $income = array_intersect_key($params, $incomeKey);
+        $model_rel = new Core_Model_Relative();
+        $search_result2 = $model_rel->search($income);
         
-       /* $model_rel = new Core_Model_Mapper_Relative();
-        $rel_with_range = array('annual_income'=>$request['annual_income']);
-        
-        $search_result2 = $model_rel->search($rel_with_range);
-        
-        $response = array_intersect($search_result, $search_result2);*/
+        $response = array_intersect($search_result, $search_result2);
         $this->_helper->logger($search_result);
-        $callback = $this->getRequest()->getParam('callback');
-        echo $callback . '(' . $this->_helper->json($search_result, false) . ')';
-        
+        //$callback = $this->getRequest()->getParam('callback');
+        //echo $callback . '(' . $this->_helper->json($search_result, false) . ')';
        
         
     }
