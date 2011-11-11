4cc030a44d4db911235a3e3aae5fd30bef46edda
2308009 <2308009@aceambala.com>
2011-11-11 06:57:11 +0000

    SEARCH WORKING

--- core/branches/educore3/application/controllers/SearchController.php
+++ core/branches/educore3/application/controllers/SearchController.php
@@ -15,30 +15,59 @@ class SearchController extends Zend_Controller_Action
     
     public function studentAction()
     {
-        $request = $this->getRequest()->getParams();
-        $model = new Core_Model_Member_Student();
         $request = $this->getRequest();
         $params = array_diff($request->getParams(), $request->getUserParams());
         $without_rangeKeys = array('gender'=>'',
                                'nationality_id'=>'',
                                'cast'=>'');
-        $with_rangeKeys = array('dob'=>'');
         $without_range = array_intersect_key($params, $without_rangeKeys);
-        $with_range = array_intersect_key($params, $with_rangeKeys);
-        //$with_range = array_diff_key($params, $without_range);
-        $this->_helper->logger($with_range);
         $this->_helper->logger($without_range);
-        $search_result = $model->search($without_range,$with_range);
+        
+        $with_rangeKeys = array('dob'=>'');
+        $with_range = array_intersect_key($params, $with_rangeKeys);
+        $this->_helper->logger($with_range);
         
         $incomeKey = array('annual_income'=>''); 
         $income = array_intersect_key($params, $incomeKey);
-        $model_rel = new Core_Model_Relative();
-        $search_result2 = $model_rel->search($income);
+        $this->_helper->logger($income);
         
-        $response = array_intersect($search_result, $search_result2);
-        $this->_helper->logger($search_result);
-        //$callback = $this->getRequest()->getParam('callback');
-        //echo $callback . '(' . $this->_helper->json($search_result, false) . ')';
+        $relSearch = false;
+        if (!empty($income)) {
+            $model_rel = new Core_Model_Relative();
+            $search_result_rel = $model_rel->search(NULL,$income);
+            $relSearch = true;
+            
+        }
+        $stuSearch = false;
+        if (!empty($without_range) or !empty($with_range)) {
+            $model = new Core_Model_Member_Student();
+            $search_result = $model->search($without_range,$with_range);
+            $stuSearch = true;
+        }
+        
+        if ($stuSearch and $relSearch) {
+            $response = array_intersect($search_result, $search_result_rel);
+        } elseif ($stuSearch)
+        {
+           $response = $search_result; 
+        } elseif ($relSearch)
+        {
+            $response = $search_result_rel; 
+        }
+        $this->_helper->logger($response);
+        $info = array();
+        foreach($response as $key => $memberId)
+        {
+            $model->setMember_id($memberId);
+            $model->initStudentInfo();
+            $info[$memberId] = array(
+            'roll_no' => $model->fetchRollNumber(),
+            'name' => $model->getFirst_name());
+        }
+       
+        $this->_helper->logger($info);
+        $callback = $this->getRequest()->getParam('callback');
+        echo $callback . '(' . $this->_helper->json($info, false) . ')';
        
         
     }
