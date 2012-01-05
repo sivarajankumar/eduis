b5607fe4536ff6760b200959297517cd909d8063
heman4t <heman4t@gmail.com>
2012-01-05 07:41:46 +0000

    Better looks and navigation

--- auth/trunk/application/views/helpers/footerList.php
+++ auth/trunk/application/views/helpers/footerList.php
@@ -1,34 +1,14 @@
 <?php
 /**
- * Sets the application logo.
+ * Sets the footer list.
  *
  */
-class Zend_View_Helper_ApplicationLogo extends Zend_View_Helper_Abstract
+class Zend_View_Helper_FooterList extends Zend_View_Helper_Abstract
 {
     /**
-     * @param string $theme
-     * @param string $img_alt
-     * @param string $class
-     * @param string $imgFormat
      * @return string
      */
-    public function applicationLogo ($theme = NULL, $img_alt = 'The College Name', $class = null, 
-    $imgFormat = 'png')
+    public function footerList ()
     {
-        $classString = NULL;
-        if (! empty($class)) {
-            $classes = NULL;
-            if (is_string($class)) {
-                $class = $this->view->escape($class);
-                $classes = array($class);
-            } else {
-                throw new Exception('The class should be string or array', 
-                Zend_Log::ERR);
-            }
-            $classString = ' class = "' . $this->view->escape($class) . '" ';
-        }
-        $imgHtmlString = '<img src="http://' . CDN_SERVER .'/images/logos/mainLogo_' . $theme . '.' . $imgFormat . '"' .
-         					'alt="' .$img_alt . '" ' . $classString . '/>';
-        return $imgHtmlString;
     }
 }
