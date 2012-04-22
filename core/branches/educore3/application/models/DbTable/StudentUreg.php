cbd5395a4388b078716eedfaf65b4d7b0e5d4ea7
2308010 <2308010@aceambala.com>
2012-04-22 10:15:52 +0000

    Concept of MemberId utilized widely now. 1.Changes to dbTable's according to new databse revision

---
+++ core/branches/educore3/application/models/DbTable/StudentUreg.php
@@ -0,0 +1,8 @@
+<?php
+class Core_Model_DbTable_StudentClass extends Zend_Db_Table_Abstract
+{
+    /**
+     * @var string Name of the database table
+     */
+    protected $_name = 'student_class';
+}
--- core/branches/educore3/application/models/DbTable/address.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Core_Model_DbTable_Address extends Zend_Db_Table_Abstract
-{
-    /**
-     * @var string Name of the database table
-     */
-    protected $_name = 'address';
-}
