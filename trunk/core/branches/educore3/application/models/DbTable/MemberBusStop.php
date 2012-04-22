2a413b59ebe0dfa23919c671b7dee58e19129fda
2308010@aceambala.com <2308010@aceambala.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2012-04-22 10:15:52 +0000

    Concept of MemberId utilized widely now. 1.Changes to dbTable's according to new databse revision

---
+++ trunk/core/branches/educore3/application/models/DbTable/MemberBusStop.php
@@ -0,0 +1,8 @@
+<?php
+class Core_Model_DbTable_MemberBusStop extends Zend_Db_Table_Abstract
+{
+    /**
+     * @var string Name of the database table
+     */
+    protected $_name = 'member_bus_stop';
+}
