e032d6f5024d3364131e1e21e1bdbbf66cf634ba
heman4t <heman4t@gmail.com>
2012-04-25 07:11:30 +0000

    a form and array sample

--- auth/trunk/application/controllers/TestController.php
+++ auth/trunk/application/controllers/TestController.php
@@ -5,14 +5,19 @@ class TestController extends Authz_Base_BaseController {
 	public function preDispatch() {return true;
 	}*/
 	public function gogoAction() {
-		$this->_helper->layout ()->enableLayout ();
-		$this->_helper->viewRenderer->setNoRender ();
-		echo 'Im in gogo';
+		$myArray = $this->_getParam('myarray');
+		print_r($myArray);
 	}
 	public function popoAction() {
 		$this->_helper->layout ()->enableLayout ();
 		$this->_helper->viewRenderer->setNoRender ();
 		echo 'Im in popo';
+	}
+	
+	public function indexAction() {
+		$this->_helper->layout ()->enableLayout ();
+		$this->_helper->viewRenderer->setNoRender (false);
+		echo 'Im in index';
 	}
 	
 
