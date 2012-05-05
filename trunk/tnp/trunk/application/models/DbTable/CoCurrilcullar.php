80dee7e34cfb8a743aa83a4d3637bafbaf1cc5e0
2308010@aceambala.com <2308010@aceambala.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2012-05-05 09:55:06 +0000

    Checked and Updated all dbtables according to the new concept of class and batch implented at core level

--- trunk/tnp/trunk/application/models/DbTable/CoCurrilcullar.php
+++ trunk/tnp/trunk/application/models/DbTable/CoCurrilcullar.php
@@ -1,8 +1,8 @@
 <?php
-class Tnp_Model_DbTable_CoCuricullar extends Zend_Db_Table_Abstract
+class Tnp_Model_DbTable_CoCurricullar extends Zend_Db_Table_Abstract
 {
 	/**
      * @var string Name of the database table
      */
-    protected $_name = 'co_curicullar';
+    protected $_name = 'co_curricullar';
 }
