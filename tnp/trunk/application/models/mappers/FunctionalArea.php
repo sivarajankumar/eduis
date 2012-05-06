b37a9b778697ed4846e6c2ba1a9447fce6075035
2308010 <2308010@aceambala.com>
2012-05-06 07:17:56 +0000

    php's inbuilt functions were not working .. issue resolved

---
+++ tnp/trunk/application/models/mappers/FunctionalArea.php
@@ -0,0 +1,81 @@
+<?php
+class Tnp_Model_Mapper_FunctionalArea
+{
+    /**
+     * @var Zend_Db_Table_Abstract
+     */
+    protected $_dbTable;
+    /**
+     * Specify Zend_Db_Table instance to use for data operations
+     * 
+     * @param  Zend_Db_Table_Abstract $dbTable 
+     * @return Tnp_Model_Mapper_FunctionalArea
+     */
+    public function setDbTable ($dbTable)
+    {
+        if (is_string($dbTable)) {
+            $dbTable = new $dbTable();
+        }
+        if (! $dbTable instanceof Zend_Db_Table_Abstract) {
+            throw new Exception('Invalid table data gateway provided');
+        }
+        $this->_dbTable = $dbTable;
+        return $this;
+    }
+    /**
+     * Get registered Zend_Db_Table instance
+     * @return Zend_Db_Table_Abstract
+     */
+    public function getDbTable ()
+    {
+        if (null === $this->_dbTable) {
+            $this->setDbTable('Tnp_Model_DbTable_FunctionalArea');
+        }
+        return $this->_dbTable;
+    }
+    /**
+     * 
+     * @param integer $functional_area_id
+     */
+    public function fetchInfo ($functional_area_id)
+    {
+        $db_table = $this->getDbTable();
+        $adapter = $db_table->getAdapter();
+        $functional_area_table = $db_table->info('name');
+        $required_cols = array('functional_area_name');
+        $select = $adapter->select()
+            ->from($functional_area_table, $required_cols)
+            ->where('functional_area_id = ?', $functional_area_id);
+        $functional_area_info = array();
+        $functional_area_info = $select->query()->fetchAll(
+        Zend_Db::FETCH_UNIQUE);
+        return $functional_area_info[$functional_area_id];
+    }
+    public function fetchFunctionalAreas ()
+    {
+        $db_table = $this->getDbTable();
+        $adapter = $db_table->getAdapter();
+        $functional_area_table = $db_table->info('name');
+        $required_cols = array('functional_area_id', 'functional_area_name');
+        $select = $adapter->select()->from($functional_area_table, $required_cols);
+        $functional_areas = array();
+        $result = array();
+        $result = $select->query()->fetchAll(Zend_Db::FETCH_UNIQUE);
+        foreach ($result as $functional_area_id => $functional_area_name_array) {
+            $functional_areas[$functional_area_id] = $functional_area_name_array['functional_area_name'];
+        }
+        return $functional_areas;
+    }
+    public function save ($prepared_data)
+    {
+        $dbtable = $this->getDbTable();
+        return $dbtable->insert($prepared_data);
+    }
+    public function update ($prepared_data, $functional_area_id)
+    {
+        $dbtable = $this->getDbTable();
+        $where = 'functional_area_id = ' . $functional_area_id;
+        return $dbtable->update($prepared_data, $where);
+    }
+}
+?>
---
+++ tnp/trunk/application/models/mappers/StudentClass.php
@@ -0,0 +1,104 @@
+<?php
+class Tnp_Model_Mapper_StudentClass
+{
+    /**
+     * @var Zend_Db_Table_Abstract
+     */
+    protected $_dbTable;
+    /**
+     * Specify Zend_Db_Table instance to use for data operations
+     * 
+     * @param  Zend_Db_Table_Abstract $dbTable 
+     * @return Tnp_Model_Mapper_StudentClass
+     */
+    public function setDbTable ($dbTable)
+    {
+        if (is_string($dbTable)) {
+            $dbTable = new $dbTable();
+        }
+        if (! $dbTable instanceof Zend_Db_Table_Abstract) {
+            throw new Exception('Invalid table data gateway provided');
+        }
+        $this->_dbTable = $dbTable;
+        return $this;
+    }
+    /**
+     * Get registered Zend_Db_Table instance
+     * @return Zend_Db_Table_Abstract
+     */
+    public function getDbTable ()
+    {
+        if (null === $this->_dbTable) {
+            $this->setDbTable('Tnp_Model_DbTable_StudentClass');
+        }
+        return $this->_dbTable;
+    }
+    /**
+     * Fetches Class details of a student
+     * 
+     * @param integer $member_id
+     */
+    public function fetchInfo ($member_id, $class_id)
+    {
+        $adapter = $this->getDbTable()->getAdapter();
+        $db_table = $this->getDbTable();
+        $stu_class_table = $db_table->info('name');
+        $required_cols = array('member_id', 'class_id', 'group_id', 'roll_no', 
+        'start_date', 'completion_date', 'is_initial_batch_identifier');
+        $select = $adapter->select()
+            ->from($stu_class_table, $required_cols)
+            ->where('member_id = ?', $member_id)
+            ->where('class_id = ?', $class_id);
+        $student_info = array();
+        $student_info = $select->query()->fetchAll(Zend_Db::FETCH_UNIQUE);
+        return $student_info[$member_id];
+    }
+    /**
+     * Fetches all Classes in which a student has/had enrolled
+     * 
+     * @param integer $member_id
+     */
+    public function fetchClassIds ($member_id)
+    {
+        $adapter = $this->getDbTable()->getAdapter();
+        $db_table = $this->getDbTable();
+        $stu_class_table = $db_table->info('name');
+        $required_cols = array('class_id');
+        $select = $adapter->select()
+            ->from($stu_class_table, $required_cols)
+            ->where('member_id = ?', $member_id);
+        $class_ids = array();
+        return $select->query()->fetchAll(Zend_Db::FETCH_COLUMN);
+    }
+    /**
+     * Fetches all Classes in which a student has/had enrolled
+     * 
+     * @param integer $member_id
+     */
+    public function fetchBatchIdentifierClassId ($member_id)
+    {
+        $adapter = $this->getDbTable()->getAdapter();
+        $db_table = $this->getDbTable();
+        $stu_class_table = $db_table->info('name');
+        $required_cols = array('class_id');
+        $select = $adapter->select()
+            ->from($stu_class_table, $required_cols)
+            ->where('is_initial_batch_identifier = ?', 1);
+        $class_id = array();
+        $class_id = $select->query()->fetchAll(Zend_Db::FETCH_COLUMN);
+        return $class_id[0];
+    }
+    public function save ($prepared_data)
+    {
+        $dbtable = $this->getDbTable();
+        return $dbtable->insert($prepared_data);
+    }
+    public function update ($prepared_data, $member_id, $class_id)
+    {
+        $dbtable = $this->getDbTable();
+        $where1 = 'member_id = ' . $member_id;
+        $where2 = 'class_id = ' . $class_id;
+        return $dbtable->update($prepared_data, array($where1, $where2));
+    }
+}
+?>
