aab1c76e60b48ca3175ca17065593b8d997f32ce
2308010 <2308010@aceambala.com>
2012-07-03 11:54:42 +0000

    corrected version number of jquery file required and coorected the code to convert false to 0 and true to 1(sql converts bool to tiny int ..)

--- admission/trunk/application/models/DbTable/Applicant.php
+++ admission/trunk/application/models/DbTable/Applicant.php
@@ -9,22 +9,30 @@ class Admsn_Model_DbTable_Applicant extends Zend_Db_Table
     }
     public function save ()
     {
+        $ever_disqualified = $this->applicant->ever_disqualified;
+        if ($ever_disqualified == false) {
+            $ever_disqualified = 0;
+        } elseif ($ever_disqualified == true) {
+            $ever_disqualified = 1;
+        }
         $applicant = array('roll_no' => $this->applicant->roll_no, 
         'application_basis' => $this->applicant->application_basis, 
         'councelling_no' => $this->applicant->councelling_no, 
         'allotted_category' => $this->applicant->allotted_category, 
         'allotted_branch' => $this->applicant->allotted_branch, 
         'state_of_domicile' => $this->applicant->state_of_domicile, 
-        'urban' => ('true' == $this->applicant->urban or true == $this->applicant->urban) ? 1 : 0, 
-        'hostel' => ('true' == $this->applicant->hostel or true == $this->applicant->hostel) ? 1 : 0, 
-        'bus' => ('true' == $this->applicant->bus or true == $this->applicant->bus) ? 1 : 0, 
-        'ever_disqualified' => $this->applicant->ever_disqualified, 
+        'urban' => ('true' == $this->applicant->urban or
+         true == $this->applicant->urban) ? 1 : 0, 
+        'hostel' => ('true' == $this->applicant->hostel or
+         true == $this->applicant->hostel) ? 1 : 0, 
+        'bus' => ('true' == $this->applicant->bus or
+         true == $this->applicant->bus) ? 1 : 0, 
+        'ever_disqualified' => $ever_disqualified, 
         'image_no' => $this->applicant->image_no, 
         'councelling_fees' => $this->applicant->councelling_fees, 
         'councelling_bank' => $this->applicant->councelling_bank, 
         'councelling_type' => $this->applicant->councelling_type, 
         'specialities' => $this->applicant->specialities);
-        
         $matric = array('roll_no' => $this->applicant->roll_no, 
         'matric_marks_obtained' => $this->applicant->matric_marks_obtained, 
         'matric_total_marks' => $this->applicant->matric_total_marks, 
@@ -35,7 +43,6 @@ class Admsn_Model_DbTable_Applicant extends Zend_Db_Table
         'matric_institution' => $this->applicant->matric_institution, 
         'matric_city' => $this->applicant->matric_city, 
         'matric_state' => $this->applicant->matric_state);
-        
         $basic = array('roll_no' => $this->applicant->roll_no, 
         'first_name' => $this->applicant->first_name, 
         'middle_name' => $this->applicant->middle_name, 
@@ -47,7 +54,6 @@ class Admsn_Model_DbTable_Applicant extends Zend_Db_Table
         'cast' => $this->applicant->cast, 
         'contact_no' => $this->applicant->contact_no, 
         'e_mail' => $this->applicant->e_mail);
-        
         $guardians = array('roll_no' => $this->applicant->roll_no, 
         'landline_no' => $this->applicant->landline_no, 
         'father_name' => $this->applicant->father_name, 
@@ -58,32 +64,29 @@ class Admsn_Model_DbTable_Applicant extends Zend_Db_Table
         'mother_name' => $this->applicant->mother_name, 
         'mother_occupation' => $this->applicant->mother_occupation, 
         'mother_designation' => $this->applicant->mother_designation, 
-        'mother_office_add' => $this->applicant->mother_office_add,
+        'mother_office_add' => $this->applicant->mother_office_add, 
         'guardian_name' => $this->applicant->guardian_name, 
         'guardian_contact' => $this->applicant->guardian_contact, 
         'guardian_relation' => $this->applicant->guardian_relation, 
         'annual_income' => $this->applicant->annual_income);
-        
         $address = array('roll_no' => $this->applicant->roll_no, 
         'c_pin' => $this->applicant->c_pin, 'c_city' => $this->applicant->c_city, 
         'c_state' => $this->applicant->c_state, 
         'c_add' => $this->applicant->c_add, 
-        'is_cp_same' => ('true' == strtolower($this->applicant->is_cp_same)  OR true==$this->applicant->is_cp_same) ? 1 : 0, 
-        'p_pin' => $this->applicant->p_pin,
-        'p_city' => $this->applicant->p_city, 
+        'is_cp_same' => ('true' == strtolower($this->applicant->is_cp_same) or
+         true == $this->applicant->is_cp_same) ? 1 : 0, 
+        'p_pin' => $this->applicant->p_pin, 'p_city' => $this->applicant->p_city, 
         'p_state' => $this->applicant->p_state, 
         'p_add' => $this->applicant->p_add, 
-        'has_guardian' => ('true' == strtolower($this->applicant->has_guardian)  OR true==$this->applicant->has_guardian) ? 1 : 0, 
-        'g_pin' => $this->applicant->g_pin,
-        'g_city' => $this->applicant->g_city, 
+        'has_guardian' => ('true' == strtolower($this->applicant->has_guardian) or
+         true == $this->applicant->has_guardian) ? 1 : 0, 
+        'g_pin' => $this->applicant->g_pin, 'g_city' => $this->applicant->g_city, 
         'g_state' => $this->applicant->g_state, 
         'g_add' => $this->applicant->g_add);
-        
         if ('null' != $this->applicant->boarding_station) {
             $bus = array('roll_no' => $this->applicant->roll_no, 
             'boarding_station' => $this->applicant->boarding_station);
         }
-        
         try {
             $this->getAdapter()->beginTransaction();
             $this->getAdapter()->insert('applicants', $applicant);
@@ -96,7 +99,6 @@ class Admsn_Model_DbTable_Applicant extends Zend_Db_Table
                     'state_cat_rank' => $this->applicant->state_cat_rank, 
                     'application_no' => $this->applicant->application_no, 
                     'aieee_marks' => $this->applicant->aieee_marks);
-                    
                     $this->getAdapter()->insert('aieee', $aieee);
                 case 'twelfth':
                     $twelfth = array('roll_no' => $this->applicant->roll_no, 
@@ -114,7 +116,6 @@ class Admsn_Model_DbTable_Applicant extends Zend_Db_Table
                     $this->getAdapter()->insert('twelfth', $twelfth);
                     break;
                 case 'leet':
-                    
                     $leet = array('roll_no' => $this->applicant->roll_no, 
                     'board_roll' => $this->applicant->board_roll, 
                     'marks_obtained' => $this->applicant->marks_obtained, 
