2976bb9082a8236f8476c585bff0132a74efc351
2308010 <2308010@aceambala.com>
2012-07-30 08:18:46 +0000

    updated skill updation facility

--- tnp/trunk/application/controllers/StudentController.php
+++ tnp/trunk/application/controllers/StudentController.php
@@ -395,7 +395,7 @@ class StudentController extends Zend_Controller_Action
      * 
      * @throws Exception
      */
-    public function getallskillsAction ()
+    public function editskillsetAction ()
     {
         $this->_helper->viewRenderer->setNoRender(false);
         $this->_helper->layout()->enableLayout();
@@ -416,11 +416,25 @@ class StudentController extends Zend_Controller_Action
         }
         $this->view->assign('all_skills', $all_skills);
     }
+    /*
+     * save new skills to database
+     */
+    public function saveskillsetAction ()
+    {
+        $this->_helper->viewRenderer->setNoRender(true);
+        $this->_helper->layout()->disableLayout();
+        $request = $this->getRequest();
+        $params = array_diff($request->getParams(), $request->getUserParams());
+        $skill_info = $params['myarray']['skill_info'];
+        $skill = new Tnp_Model_Skill();
+        $skill_data = array('skill_name' => $skill_info['skill_name'], 
+        'skill_field' => $skill_info['skill_field']);
+        $skill_id = $skill->saveInfo($skill_data);
+    }
     /**
      * Enables user to view his skills
      * @deprecated coded inline inside profile action
      */
-    
     /*public function viewskillsAction ()
     {
         $this->_helper->viewRenderer->setNoRender(false);
@@ -443,6 +457,11 @@ class StudentController extends Zend_Controller_Action
      */
     public function editskillsAction ()
     {
+        $this->_helper->viewRenderer->setNoRender(false);
+        $this->_helper->layout()->enableLayout();
+    }
+    public function saveskillsAction ()
+    {
         $this->_helper->viewRenderer->setNoRender(true);
         $this->_helper->layout()->disableLayout();
         $request = $this->getRequest();
@@ -460,13 +479,9 @@ class StudentController extends Zend_Controller_Action
          * if skill does not exist in databse add it, otherwise update member's proficiency
          */
         if ($is_new_skill == 'true') {
-            $skill = new Tnp_Model_Skill();
-            $skill_data = array('skill_name' => $skill_info['skill_name'], 
-            'skill_field' => $skill_info['skill_field']);
             /*
-             * skill id generated for new skill and $skill id updated
+             * redirect to saveskillset
              */
-            $skill_id = $skill->saveInfo($skill_data);
         }
         $member_id = $this->getMember_id();
         $student = new Tnp_Model_Member_Student();
