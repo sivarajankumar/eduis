b95fc97310efa6f0961cb129458cea1eea1f5a92
2308010 <2308010@aceambala.com>
2012-09-21 11:21:29 +0000

    index controller redirects the user to tnp/admin if user is mgmt

--- core/branches/educore3/application/controllers/IndexController.php
+++ core/branches/educore3/application/controllers/IndexController.php
@@ -8,10 +8,84 @@
 class IndexController extends Zend_Controller_Action
 {
     /**
-     * The default action - show the home page
+     * 
+     * @var int
      */
+    protected $_member_id;
+    protected $_user_name;
+    protected $_user_type;
+    protected $_department_id;
+    /**
+     * @return the $_member_id
+     */
+    protected function getMember_id ()
+    {
+        return $this->_member_id;
+    }
+    /**
+     * @return the $_user_name
+     */
+    protected function getUser_name ()
+    {
+        return $this->_user_name;
+    }
+    /**
+     * @return the $_user_type
+     */
+    protected function getUser_type ()
+    {
+        return $this->_user_type;
+    }
+    /**
+     * @return the $_department_id
+     */
+    protected function getDepartment_id ()
+    {
+        return $this->_department_id;
+    }
+    /**
+     * @param int $_member_id
+     */
+    protected function setMember_id ($_member_id)
+    {
+        $this->_member_id = $_member_id;
+    }
+    /**
+     * @param field_type $_user_name
+     */
+    protected function setUser_name ($_user_name)
+    {
+        $this->_user_name = $_user_name;
+    }
+    /**
+     * @param field_type $_user_type
+     */
+    protected function setUser_type ($_user_type)
+    {
+        $this->_user_type = $_user_type;
+    }
+    /**
+     * @param field_type $_department_id
+     */
+    protected function setDepartment_id ($_department_id)
+    {
+        $this->_department_id = $_department_id;
+    }
+    public function init ()
+    {
+        if (Zend_Auth::getInstance()->hasIdentity()) {
+            $authInfo = Zend_Auth::getInstance()->getStorage()->read();
+            $this->setDepartment_id($authInfo['department_id']);
+            $this->setUser_name($authInfo['identity']);
+            $this->setUser_type($authInfo['userType']);
+            $this->setMember_id($authInfo['member_id']);
+        }
+    }
     public function indexAction ()
     {
+        if ($this->_department_id == 'MGMT' or $this->_department_id == 'mgmt') {
+            $this->_redirect('http://' . TNP_SERVER . '/admin');
+        }
     }
 }
 ?>
