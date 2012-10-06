d90458f483b3f4c6d2b2b4abac9a68dc40d982bf
2308010@aceambala.com <2308010@aceambala.com@576e4884-5f5a-85fa-688c-129c97d2be85>
2012-10-06 06:33:13 +0000

    Company model mapper dbtable addded Careeer controller added

--- trunk/tnp/trunk/application/controllers/CareerController.php
+++ trunk/tnp/trunk/application/controllers/CareerController.php
@@ -1,18 +1,123 @@
 <?php
 class CareerController extends Zend_Controller_Action
 {
-    public function init ()
+    /**
+     * 
+     * @var int
+     */
+    protected $_member_id;
+    protected $_user_name;
+    protected $_user_type;
+    protected $_department_id;
+    /**
+     * @return the $_member_id
+     */
+    protected function getMember_id ()
     {
-        /* Initialize action controller here */
+        return $this->_member_id;
+    }
+    /**
+     * @return the $_user_name
+     */
+    protected function getUser_name ()
+    {
+        return $this->_user_name;
+    }
+    /**
+     * @return the $_user_type
+     */
+    protected function getUser_type ()
+    {
+        return $this->_user_type;
+    }
+    /**
+     * @return the $_department_id
+     */
+    protected function getDepartment_id ()
+    {
+        return $this->_department_id;
+    }
+    /**
+     * @param int $_member_id
+     */
+    protected function setMember_id ($_member_id)
+    {
+        $this->_member_id = $_member_id;
+    }
+    /**
+     * @param field_type $_user_name
+     */
+    protected function setUser_name ($_user_name)
+    {
+        $this->_user_name = $_user_name;
+    }
+    /**
+     * @param field_type $_user_type
+     */
+    protected function setUser_type ($_user_type)
+    {
+        $this->_user_type = $_user_type;
+    }
+    /**
+     * @param field_type $_department_id
+     */
+    protected function setDepartment_id ($_department_id)
+    {
+        $this->_department_id = $_department_id;
     }
     public function indexAction ()
     {
-        // action body
+        $this->_helper->viewRenderer->setNoRender(false);
+        $this->_helper->layout()->enableLayout();
+    }
+    public function init ()
+    {
+        if (Zend_Auth::getInstance()->hasIdentity()) {
+            $authInfo = Zend_Auth::getInstance()->getStorage()->read();
+            $this->setDepartment_id($authInfo['department_id']);
+            $this->setUser_name($authInfo['identity']);
+            $this->setUser_type($authInfo['userType']);
+            $this->setMember_id($authInfo['member_id']);
+        }
     }
     public function placementrecordAction ()
     {
         $this->_helper->viewRenderer->setNoRender(false);
         $this->_helper->layout()->enableLayout();
+    }
+    public function fetchcompaniesAction ()
+    {
+        $this->_helper->viewRenderer->setNoRender(true);
+        $this->_helper->layout()->disableLayout();
+        $format = $this->_getParam('format', 'log');
+        $companies = $this->fetchCompanies();
+        switch ($format) {
+            case 'html':
+                $this->_helper->viewRenderer->setNoRender(false);
+                $this->_helper->layout()->enableLayout();
+                $this->view->assign('companies', $companies);
+                break;
+            case 'jsonp':
+                $callback = $this->getRequest()->getParam('callback');
+                echo $callback . '(' . $this->_helper->json($companies, false) .
+                 ')';
+                break;
+            case 'json':
+                $this->_helper->json($companies);
+                break;
+            case 'log':
+                Zend_Registry::get('logger')->debug('No format was provided..');
+                Zend_Registry::get('logger')->debug($companies);
+                break;
+            default:
+                ;
+                break;
+        }
+    }
+    private function fetchCompanies ()
+    {
+        $company = new Tnp_Model_Company();
+        return $company->fetchCompanies();
     }
 }
 
